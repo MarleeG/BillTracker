e7e11db73aebdfce68615fa89695bae201e68cdc
Christina Gerard <ChristinaGerard@Christinas-MacBook-Pro.local>
2019-02-12 16:45:48 -0500

    updated in jquery

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import Form from './components/form';
+import './App.css'
 
 class App extends Component {
   state = {
-      he: 'hello'
-    }
+    title: 'Track Bills',
+    bills: []
+  }
 
-    
+
   render() {
-    
+
     return (
-      <div>
-        <h1>{this.state.he}</h1>
-        <Form />
+      <div className='app-body'>
+        <h1 className='text-center title'>{this.state.title}</h1>
+        <Form bills={this.state.bills}/>
       </div>
     );
   }
--- src/components/form.js
+++ src/components/form.js
@@ -1,45 +1,92 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Modal, Container, Row, Col } from 'react-bootstrap';
+import './components.css'
 
 class Form extends Component {
-
-
     state = {
-        showForm: false
+        showForm: false,
+        lgShow: true,
     }
 
     handleSubmit = () => {
         this.setState({
             showForm: true,
-            company: ''
+            company: '',
+        })
+    }
+
+    handleHide = () => {
+        this.setState({
+            showForm: false
         })
     }
 
     handleChange = (event) => {
-
-        const {name, value} = event.target;
-
+        const { name, value } = event.target;
         this.setState({
             [name]: value
-        }, () => console.log(`after state set => ${this.state.company}`))
+        })
+    }
+
+    form = () => {
+        return (
+            <div className='text-center'>
+                <Modal
+                    size="lg"
+                    show={this.state.showForm}
+                    onHide={this.handleHide}
+                    aria-labelledby="example-modal-sizes-title-lg"
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title id="example-modal-sizes-title-lg">
+                            Adding Bill
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Container>
+                            <Row className="show-grid">
+                                <Col xs={12} md={8}>
+                                    <code>.col-xs-12 .col-md-8</code>
+                                </Col>
+                                <Col xs={6} md={4}>
+                                    <code>.col-xs-6 .col-md-4</code>
+                                </Col>
+                            </Row>
+
+                            <Row className="show-grid">
+                                <Col xs={6} md={4}>
+                                    <code>.col-xs-6 .col-md-4</code>
+                                </Col>
+                                <Col xs={6} md={4}>
+                                    <code>.col-xs-6 .col-md-4</code>
+                                </Col>
+                                <Col xs={6} md={4}>
+                                    <code>.col-xs-6 .col-md-4</code>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Modal.Body>
+                </Modal>
+            </div>
+        )
     }
 
     render() {
+
         return (
             <div>
-                <Button onClick={this.handleSubmit} variant="primary">Add Bill</Button>
+                <div className='text-center btn-add-bill'>
+                    <Button onClick={this.handleSubmit} variant="warning" size='lg'> Add Bill </Button>
+                    <br />
+                    <br />
+                    <Button onClick={this.handleSubmit} variant="warning" size='lg'> See Bills </Button>
+                </div>
 
-                { 
-                    this.state.showForm &&  
-                    <form>
-                        <label>
-                            Company:
-                        <input type="text" name="company" onChange={this.handleChange}/>
-                        </label>
-                        <input type="submit" value="Submit" />
-                    </form>
+                {
+                    this.state.showForm &&
+                    this.form()
                 }
-               
+
             </div>
         )
     }
